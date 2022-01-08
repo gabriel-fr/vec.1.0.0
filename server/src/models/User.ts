f1f02198a766adda858de458ec7c121d4474022e
marceloambarc <marcelo.ambarc@gmail.com>
2022-01-08 16:53:52 -0300

    Models

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToOne, OneToMany, JoinColumn } from 'typeorm';
 import { Profile } from './Profile';
-import { Album } from "./Album";
 import { Article } from './Article';
+import { Album } from "./Album";
 
 @Entity()
 export class User {
@@ -19,10 +19,10 @@ export class User {
   @JoinColumn()
   profile: Profile;
 
-  @OneToMany(() => Album, album => album.user)
-  albuns: Album[];
-
   @OneToMany(() => Article, article => article.user)
   articles: Article[];
+
+  @OneToMany(() => Album, album => album.user)
+  albuns: Album[];
 
 }
