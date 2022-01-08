d0a2cd04015d2032b80403e97ce3fcd13538d6cc
Gabriel Ribeiro <gabriel@enzo.com.br>
2022-01-08 11:41:29 -0300

    updates

---
+++ web/src/Home.js
@@ -0,0 +1,11 @@
+import { Header } from "./components/styles";
+
+function Home() {
+  return (
+    <>
+      <Header>Vivendo em Cristo</Header>
+    </>
+  );
+}
+
+export default Home;
---
+++ web/src/components/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  background-image: url(.././images/a3M6PH.jpg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-attachment: fixed;
+`;
